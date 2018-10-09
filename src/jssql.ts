2103adc122d6cad07296200e6d7d7f5f48a340bc
liwei <liwei@youzan.com>
2018-10-09 17:41:42 +0800

    feat:insert

--- src/jssql.ts
+++ src/jssql.ts
@@ -18,8 +18,29 @@ export function jssql(config) {
                 }
             }, () => connection.query(sql.join(' ')));
         },
-        insert(values) {
-            
+        insert(rows) {
+            const keys = [];
+            const values = [];
+            rows.forEach(row => {
+                const value = [];
+                Object.keys(row).forEach(key => {
+                    if (keys.indexOf(key) === -1) {
+                        keys.push(key);
+                    }
+                    const index = keys.indexOf(key);
+                    value[index] = row[key];
+
+                });
+                values.push(values);
+            });
+            const sql = [`INSERT (${connection.escape(keys)}) INTO`];
+            return createTrigger({
+                into(table) {
+                    sql.push(table);
+                    sql.push(`(${connection.escape(values)})`);
+                    return this;
+                }
+            }, () => connection.query(sql.join(' ')));
         },
         select() {
 
