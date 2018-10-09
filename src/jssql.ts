63268c4daf4067b245b7090611a697f874d992a2
liwei <liwei@youzan.com>
2018-10-09 17:22:09 +0800

    feat:count

--- src/jssql.ts
+++ src/jssql.ts
@@ -1,16 +1,25 @@
 import mysql from 'mysql';
 import { createTrigger } from './trigger';
+import { buildWhere } from './builder';
 export function jssql(config) {
     const connection = mysql.createConnection(config);
     connection.connect();
     return {
         connection,
-        count() {
-
+        count(table) {
+            const sql = [`SELECT COUNT(*) FROM ${table}`];
+            return createTrigger({
+                where(and, or) {
+                    const where = buildWhere(and, or);
+                    if (sql.indexOf(where) === -1) {
+                        sql.push(where);
+                    }
+                    return this;
+                }
+            }, () => connection.query(sql.join(' ')));
         },
-        insert() {
-            const sql = [];
-            createTrigger({}, connection.query(sql.join(' ')));
+        insert(values) {
+            
         },
         select() {
 
